7b8959b7bf6dd57177810de0f4d4200198e94c4d
Berk Ozturk <34316304+berkozturk97@users.noreply.github.com>
2020-12-14 22:26:30 +0300

    v4

--- src/hook/useStateCallback.js
+++ src/hook/useStateCallback.js
@@ -2,18 +2,17 @@ import React, { useState, useRef, useEffect } from 'react';
 
 function useStateCallback(initialState) {
     const [state, setState] = useState(initialState);
-    const cbRef = useRef(null); // mutable ref to store current callback
+    const cbRef = useRef(null);
   
     const setStateCallback = (state, cb) => {
-      cbRef.current = cb; // store passed callback to ref
+      cbRef.current = cb;
       setState(state);
     };
   
     useEffect(() => {
-      // cb.current is `null` on initial render, so we only execute cb on state *updates*
       if (cbRef.current) {
         cbRef.current(state);
-        cbRef.current = null; // reset callback after execution
+        cbRef.current = null;
       }
     }, [state]);
   
